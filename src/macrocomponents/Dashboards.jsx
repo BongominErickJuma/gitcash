e480e69273ef37ab7933322a39460cb35d8f792c
BongominErickJuma <ericbongomin@gmail.com>
2024-08-13 11:41:51 +0300

    modified:   src/index.css 	modified:   src/macrocomponents/Dashboards.jsx

--- src/macrocomponents/Dashboards.jsx
+++ src/macrocomponents/Dashboards.jsx
@@ -5,24 +5,19 @@ import miniSnippet from "../assets/dash-small.jpg";
 const Dashboards = () => {
   return (
     <section id="features" className="features section mt-0 p-0">
-      <div className="container position-relative">
-        <div className="features-image col-lg-12 shadow">
+      <div className="container image-container ">
+        <div className="features-image col-lg-12">
           <img
             src={codeSnippet}
             alt="vs-code snippet"
             className="main-img rounded-2"
           />
         </div>
-        <div
-          className="features-image col-lg-4 position-absolute  end-0 borrom-0 top-50 w-50 shadow-lg"
-          style={{
-            height: "500px",
-          }}
-        >
+        <div className="features-mini-image col-lg-4">
           <img
             src={miniSnippet}
             alt="vs-code snippet"
-            className="main-img rounded-2"
+            className="mini-img rounded-2"
           />
         </div>
       </div>
